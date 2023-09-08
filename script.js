0509c4dd1c97dbe42772b0ffca17c1f61bff596d
LogolicusYZ <adjordbra@gmail.com>
2023-09-08 10:43:35 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
